622878a06cf284057da7485cf2a42cc5d7d51c00
이정민(Jungmin Lee)/Payment개발팀/11ST <lcalmsky@gmail.com>
2019-11-27 09:20:10 +0900

    Add @AfterEach, @AfterAll, @DisplayName, @Disabled examples

--- junit/src/test/java/io/lcalmsky/junit/BeforeTests.java
+++ junit/src/test/java/io/lcalmsky/junit/BeforeTests.java
@@ -1,11 +1,8 @@
 package io.lcalmsky.junit;
 
 import lombok.extern.java.Log;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Log
 public class BeforeTests {
@@ -17,17 +14,5 @@ public class BeforeTests {
     @BeforeEach
     public void init() {
         log.info("@BeforeEach");
-    }
-
-    @Test
-    public void one() {
-        log.info("one: executed");
-        assertTrue(true);
-    }
-
-    @Test
-    public void two() {
-        log.info("two: executed");
-        assertTrue(true);
     }
 }
