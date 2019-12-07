4f1441ccdfe36827c1e3a0af19577cb28237029a
Jungmin Lee <lcalmsky@gmail.com>
2019-12-08 02:55:39 +0900

    Update the JUnit test version to 5.x
    - @JsonInject

--- jackson/src/test/java/io/lcalmsky/jackson/domain/BeanWithJacksonInjectTests.java
+++ jackson/src/test/java/io/lcalmsky/jackson/domain/BeanWithJacksonInjectTests.java
@@ -3,24 +3,31 @@ package io.lcalmsky.jackson.domain;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BeanWithJacksonInjectTests {
     @Test
-    public void whenJsonWithoutSomeFieldProvided_thenInjectAndDeserialize_expectCorrect() throws JsonProcessingException {
-        // when
+    @DisplayName("JsonInjection필드에 값 주입하기")
+    public void givenJsonWithoutSomeFieldProvided_whenInjectAndDeserialize_thenCorrect() throws JsonProcessingException {
+        // given
         String json = "{\"name\":\"name\"}";
 
-        // then
+        // when
         InjectableValues injectableValues = new InjectableValues.Std().addValue(int.class, 1);
         BeanWithJacksonInject bean = new ObjectMapper().reader(injectableValues).forType(BeanWithJacksonInject.class).readValue(json);
 
-        // expect
+        // then
+        assertAll(
+                () -> assertEquals(1, bean.getId()),
+                () -> assertEquals("name", bean.getName())
+        );
+
+        // log
         System.out.println(json);
         System.out.println(bean);
-        assertEquals(1, bean.getId());
-        assertEquals("name", bean.getName());
     }
 }
