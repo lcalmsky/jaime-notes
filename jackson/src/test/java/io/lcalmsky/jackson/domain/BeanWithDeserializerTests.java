6557c38a9d538c041b35b55c9210b8fb635badb1
Jungmin Lee <lcalmsky@gmail.com>
2019-12-07 15:41:56 +0900

    Update the JUnit test version to 5.x
    - @JsonDeserializer

--- jackson/src/test/java/io/lcalmsky/jackson/domain/BeanWithDeserializerTests.java
+++ jackson/src/test/java/io/lcalmsky/jackson/domain/BeanWithDeserializerTests.java
@@ -2,7 +2,8 @@ package io.lcalmsky.jackson.domain;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.text.SimpleDateFormat;
 
@@ -10,15 +11,19 @@ import static org.junit.Assert.assertEquals;
 
 public class BeanWithDeserializerTests {
     @Test
-    public void WhenBeanWithDeserializerProvided_ThenDeserialize_ExpectCorrect() throws JsonProcessingException {
-        // when
+    @DisplayName("Deserializer를 지정하여 날짜형식을 역직렬화함")
+    public void givenBeanWithDeserializerProvided_whenDeserialize_thenCorrect() throws JsonProcessingException {
+        // given
         String json = "{\"name\":\"today\",\"date\":\"2019-11-19 15:45:00\"}";
 
-        // then
+        // when
         BeanWithDeserializer bean = new ObjectMapper().readValue(json, BeanWithDeserializer.class);
 
-        // expected
+        // then
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         assertEquals("2019-11-19 15:45:00", format.format(bean.getDate()));
+
+        // log
+        System.out.println(bean);
     }
 }
