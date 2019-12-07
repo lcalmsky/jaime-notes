b6b6520f05834dff91d29f61e32e38faf392e884
이정민(Jungmin Lee)/Payment개발팀/11ST <lcalmsky@gmail.com>
2019-12-07 15:32:41 +0900

    Update the JUnit test version to 5.x
    - @JsonAlias

--- jackson/src/test/java/io/lcalmsky/jackson/domain/BeanWithAliasTests.java
+++ jackson/src/test/java/io/lcalmsky/jackson/domain/BeanWithAliasTests.java
@@ -2,29 +2,36 @@ package io.lcalmsky.jackson.domain;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BeanWithAliasTests {
     @Test
-    public void WhenJsonAliasProvided_ThenDeserialize_ExpectCorrect() throws JsonProcessingException {
-        // when
+    @DisplayName("@JsonAlias에 추가한 필드명으로 JSON을 구성하여 역직렬화하여도 같은 결과를 획득")
+    public void givenJsonAliasProvided_whenDeserialize_thenCorrect() throws JsonProcessingException {
+        // given
         String json1 = "{\"firstName\":\"Jungmin\",\"lastName\":\"Lee\"}";
         String json2 = "{\"first_name\":\"Jungmin\",\"lastName\":\"Lee\"}";
         String json3 = "{\"fName\":\"Jungmin\",\"lastName\":\"Lee\"}";
 
-        // then
+        // when
         ObjectMapper objectMapper = new ObjectMapper();
         BeanWithAlias bean1 = objectMapper.readValue(json1, BeanWithAlias.class);
         BeanWithAlias bean2 = objectMapper.readValue(json2, BeanWithAlias.class);
         BeanWithAlias bean3 = objectMapper.readValue(json3, BeanWithAlias.class);
 
-        // expected
+        // then
+        assertAll(
+                () -> assertEquals(bean1.getFirstName(), bean2.getFirstName()),
+                () -> assertEquals(bean2.getFirstName(), bean3.getFirstName())
+        );
+
+        // log
         System.out.println(bean1);
         System.out.println(bean2);
         System.out.println(bean3);
-        assertEquals(bean1.getFirstName(), bean2.getFirstName());
-        assertEquals(bean2.getFirstName(), bean3.getFirstName());
     }
 }
