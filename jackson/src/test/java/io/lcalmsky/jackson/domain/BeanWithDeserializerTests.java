3509b4e22a283ab892ce34f9b9dd1002994cb058
Jungmin Lee <lcalmsky@gmail.com>
2019-12-08 03:06:04 +0900

    Update the JUnit test version to 5.x
    - BeanWithDeserializerTests.java
    - BeanWithJsonSetterTests.java

--- jackson/src/test/java/io/lcalmsky/jackson/domain/BeanWithDeserializerTests.java
+++ jackson/src/test/java/io/lcalmsky/jackson/domain/BeanWithDeserializerTests.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test;
 
 import java.text.SimpleDateFormat;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class BeanWithDeserializerTests {
     @Test
--- jackson/src/test/java/io/lcalmsky/jackson/domain/BeanWithJsonSetterTests.java
+++ jackson/src/test/java/io/lcalmsky/jackson/domain/BeanWithJsonSetterTests.java
@@ -2,24 +2,29 @@ package io.lcalmsky.jackson.domain;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class BeanWithJsonSetterTests {
     @Test
-    public void WhenJsonSetterAppliedAndJsonProvided_ThenDeserialize_ExpectCorrect() throws JsonProcessingException {
-        // when
+    @DisplayName("JsonSetter를 이용하여 역직렬화하기")
+    public void givenJsonSetterAppliedAndJsonProvided_whenDeserialize_thenCorrect() throws JsonProcessingException {
+        // given
         String json = "{\n" +
                 "  \"id\": 1,\n" +
                 "  \"myName\": \"Jungmin Lee\"\n" +
                 "}";
 
-        // then
+        // when
         BeanWithJsonSetter bean = new ObjectMapper().readValue(json, BeanWithJsonSetter.class);
 
-        // expect
+        // then
         assertEquals("Jungmin Lee", bean.getName());
+
+        // log
         System.out.println(json);
         System.out.println(bean);
     }
