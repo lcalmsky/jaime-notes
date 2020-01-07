f402cb6cb7e20919706cece343637883df5ffec7
Jungmin Lee <lcalmsky@gmail.com>
2020-01-07 15:06:05 +0900

    CRA - autoprefixer

--- react/cra-test/src/App.js
+++ react/cra-test/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList';
+import './test.css';
 
 console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
 console.log(`REACT_APP_DATA_API = ${process.env.REACT_APP_DATA_API}`);
