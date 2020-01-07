71442fd24f1c03b1f71febae34861b528ebe78ff
Jungmin Lee <lcalmsky@gmail.com>
2020-01-07 15:01:36 +0900

    CRA - 환경 변수 사용하기

--- react/cra-test/src/App.js
+++ react/cra-test/src/App.js
@@ -3,15 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList';
 
+console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
+console.log(`REACT_APP_DATA_API = ${process.env.REACT_APP_DATA_API}`);
+
 function App() {
-  return (
-      <div className="App">
-          <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo"/>
-              <p>
-                  Test
-              </p>
-              <a
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Test
+                </p>
+                <a
                   className="App-link"
                   href="https://reactjs.org"
                   target="_blank"
